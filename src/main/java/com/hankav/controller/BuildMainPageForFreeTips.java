1d899c5e6ff1691ebe8fbeb6dca12480ce81b51f
hankavtech <47284630+hankavtech@users.noreply.github.com>
2019-03-21 23:52:57 +0000

    trying to reach home page

--- src/main/java/com/hankav/controller/BuildMainPageForFreeTips.java
+++ src/main/java/com/hankav/controller/BuildMainPageForFreeTips.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import com.hankav.dao.getTipsterRatings;
 /**
  * Servlet implementation class buildRankingContent
  */
+@WebServlet("/")
 public class BuildMainPageForFreeTips extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
